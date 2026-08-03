35841d45400ab7a9f845ba05f721d64b252b5427
Omar4346 <20omarr04@gmail.com>
2026-08-03 01:21:38 -0700

    update to api service and globals

--- angular_frontend/src/app/api-service/api-service.ts
+++ angular_frontend/src/app/api-service/api-service.ts
@@ -2,6 +2,17 @@ import { Component, } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { GlobalCnst, GlobalMod } from '../globals';
 
+const responseMsg: { [key: string]: string } = {
+  200: "New York",
+  201: "California",
+  400: "BAD REQUEST",
+  401: "UNAUTHORIZED",
+  403: "FORBIDDEN",
+  404: "NOT FOUND",
+  500: "INTERNAL SERVER ERROR"
+};
+
+
 @Component({
   selector: 'app-api-service',
   imports: [],
@@ -16,8 +27,12 @@ export class ApiService {
   //make request, handle response, if content: return, if arg:
   async apiRequest(requestType: string, args:JSON){
     let response = this.http.post(GlobalCnst.BASE_API_URL + requestType, args)
+    //no return, throw error
     if (response == null){
 
     }
+  
+  //decrypt and return if successful
+  
   }
 }
--- angular_frontend/src/app/globals.ts
+++
@@ -1,8 +0,0 @@
-export const GlobalCnst = Object.freeze({
-     BASE_API_URL: 'http://example.com/',
-});
-
-export const GlobalMod = {
-    sessionKey: "",
-    username: ""
-}
