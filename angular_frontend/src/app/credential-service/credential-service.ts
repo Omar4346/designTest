e6f7a09c441d3de7132f60c94e915ae8b25d2937
Omar4346 <20omarr04@gmail.com>
2026-08-08 02:04:16 -0700

    update to login

--- angular_frontend/src/app/credential-service/credential-service.ts
+++ angular_frontend/src/app/credential-service/credential-service.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ApiService} from '../api-service/api-service';
-import { createMlKem768 } from "mlkem";
+import { createMlKem768, MlKemInterface} from "mlkem";
 
 interface user {
     firstName: string,
@@ -34,14 +34,16 @@ export class CredentialService {
   private clientPublicKey = new Uint8Array();
   private clientPrivateKey = new Uint8Array();
   private serverPublicKey = new Uint8Array();
+  private recipient: Awaited<ReturnType<typeof createMlKem768>>;
 
-  private recipient = new Promise((resolveOuter) => {
-  resolveOuter(
-    new Promise((resolveInner) => {
-      setTimeout(resolveInner, 1000);
-    }),
-  );
-});
+  private constructor(recipient: Awaited<ReturnType<typeof createMlKem768>>) {
+    this.recipient = recipient;
+  }
+
+  static async create(): Promise<CredentialService> {
+    const recipient = await createMlKem768();
+    return new CredentialService(recipient);
+  }
 
   async request_public_key(){
     let keyResponse:{[index: string]:any} = this.Api.apiRequest("GET", "publicKey");
@@ -52,7 +54,32 @@ export class CredentialService {
 
   async encrypt(input:JSON){
     //encrypt input, return
+    const recipient = await CredentialService.create();
+
     let [kemCiphertext, sharedSecret] = recipient.encap(this.clientPublicKey)
+    let toEncrypt = Buffer.from(JSON.stringify(input), "utf8");
+
+
+    let iv = randomBytes(12),
+    cipher = createCipheriv(
+      "aes-256-gcm",
+      Buffer.from(sharedSecret),
+      iv
+    );
+    let encrypted = Buffer.concat([
+      cipher.update(toEncrypt),
+      cipher.final(),
+    ]);
+    let tag = cipher.getAuthTag();
+    let message = {
+      kemCiphertext,
+      iv,
+      tag,
+      encrypted,
+    };
+    return message
+
+
   }
 
   //decrypt():
@@ -60,7 +87,7 @@ export class CredentialService {
   }
 
   async initClientKeys(){
-    let recipient = await createMlKem768();
-    [this.clientPublicKey, this.clientPrivateKey] = recipient.generateKeyPair();
+    const credentialService = await CredentialService.create();
+    [this.clientPublicKey, this.clientPrivateKey] = this.recipient.generateKeyPair();
   }
 }
